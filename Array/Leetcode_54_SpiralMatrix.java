4e8976e1b54b8a63cf92ed57fa130c0bc3151ca3
Shiva181006 <shivakasaudhan817@gmail.com>
2026-06-09 22:06:05 +0530

    Spiral Matrix

---
+++ Array/Leetcode_54_SpiralMatrix.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Leetcode_54_SpiralMatrix {
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+
+        while (top <= bottom && left <= right) {
+
+            // Traverse left to right
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+
+            // Traverse top to bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+
+            // Traverse right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+
+            // Traverse bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+
+        List<Integer> result = spiralOrder(matrix);
+
+        System.out.println(result);
+    }
+}
