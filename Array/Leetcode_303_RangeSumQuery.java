6e475d6b5d7cc839cee48e53512b792c56353ee0
Shiva181006 <shivakasaudhan817@gmail.com>
2026-06-25 23:10:22 +0530

    Range Sum Query

---
+++ Array/Leetcode_303_RangeSumQuery.java
@@ -0,0 +1,35 @@
+public class Leetcode_303_RangeSumQuery{
+
+    static class NumArray {
+
+        int[] prefix;
+
+        public NumArray(int[] nums) {
+
+            prefix = new int[nums.length + 1];
+
+            for (int i = 0; i < nums.length; i++) {
+                prefix[i + 1] = prefix[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+
+            return prefix[right + 1] - prefix[left];
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+
+        NumArray obj = new NumArray(nums);
+
+        System.out.println(obj.sumRange(0, 2)); // Output: 1
+
+        System.out.println(obj.sumRange(2, 5)); // Output: -1
+
+        System.out.println(obj.sumRange(0, 5)); // Output: -3
+    }
+}
