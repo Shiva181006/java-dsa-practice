c047ce956f2da64fb514b65effe5c6aa5e3290f2
Shiva181006 <shivakasaudhan817@gmail.com>
2026-07-14 21:09:35 +0530

    Solve Assign Cookies using while loop

---
+++ Array/Leetcode_455_AssignCookies.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Leetcode_455_AssignCookies{
+
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (s[cookie] >= g[child]) {
+                child++;
+            }
+            cookie++;
+        }
+
+        return child;
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1, 2, 3};
+        int[] s = {1, 1};
+
+        int result = findContentChildren(g, s);
+        System.out.println("Maximum content children: " + result);
+    }
+}
